c04f6468ef522ac0e38e0d7f946f0593cacd3fab
engrmrdc <128046637+engrmrdc@users.noreply.github.com>
2024-05-28 15:58:20 +0800

    Update Life-in-Pixels.py

--- Life-in-Pixels.py
+++ Life-in-Pixels.py
@@ -1,3 +1,19 @@
+# Life in Pixels
+#
+# Computer screens display images by representing each pixel with three color values: red, green, and blue (RGB). Each color is represented by an integer value ranging from 0 to 255.
+# 
+# Develop a function named string_to_rgb that takes a string and returns a list of three integers representing the red, green, and blue values of the string's RGB color. The function should return [0, 0, 0] if the input string is empty or if it contains any non-alphabetic characters.
+# 
+# when mapping a string to RGB values:
+# 
+# 1. Each letter is assigned a numeric value based on its position in the alphabet. (a is 1, b is 2 and so on)
+# 2. This numeric value is multiplied by specific factors (1 for red, 2 for green, and 3 for blue).
+# 3. The resulting values are accumulated for the red, green, and blue components.
+# 4. Ensure each component stays within the range of 0 to 255.
+# 5. The final RGB color is returned as a list: [red, green, blue]
+#
+# ** The mapping is case-insensitive, so both uppercase and lowercase letters are treated the same.
+
 import string
 def string_to_rgb(input_string):
     res = 0
@@ -12,6 +28,8 @@ def string_to_rgb(input_string):
         print(rgb)
     else:
         print([0, 0, 0])
+
+# Under this line are the data for testing.
 
 # Input and Expected Outputs
 # input_string = 'XYZ'
